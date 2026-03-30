13d85959da777648614007af7547ab025d9f8a1b
Ali Ahsan <aliahsan8751@gmail.com>
2026-03-30 11:16:12 +0500

    added blogs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,22 @@ import { KeyboardInputComponent } from './features/game/components/keyboard-inpu
 import { HomeComponent } from './shared/home/home.component';
 import { GameComponent } from './features/game/components/game/game.component';
 import { PrivacyComponent } from './shared/privacy/privacy.component';
+import { AllBlogsComponent } from './core/blogs/all-blogs/all-blogs.component';
+import { ImproveTypingSpeedComponent } from './core/blogs/improve-typing-speed/improve-typing-speed.component';
+import { ScienceBehindAccuracyComponent } from './core/blogs/science-behind-accuracy/science-behind-accuracy.component';
+import { TopOnlineTypingGamesComponent } from './core/blogs/top-online-typing-games/top-online-typing-games.component';
+import { TypingChallengeComponent } from './core/blogs/typing-challenge/typing-challenge.component';
+import { TypingGameComponent } from './core/blogs/typing-game/typing-game.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'game', component: GameComponent},
     {path: 'keyboard', component: KeyboardInputComponent},
     {path: 'privacy', component: PrivacyComponent},
+    {path: 'typing-blogs', component: AllBlogsComponent},
+    {path: 'improve-typing-speed', component: ImproveTypingSpeedComponent},
+    {path: 'sciece-behind-accuracy', component: ScienceBehindAccuracyComponent},
+    {path: 'top-online-typing-games', component: TopOnlineTypingGamesComponent},
+    {path: 'typing-challenge', component: TypingChallengeComponent},
+    {path: 'typing-game', component: TypingGameComponent}, 
 ];
---
+++ src/app/core/blogs/all-blogs/all-blogs.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+interface Blog {
+  title: string;
+  thumbnail: string;
+  publishedDate: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-all-blogs',
+  imports:[RouterModule, CommonModule],
+  templateUrl: './all-blogs.component.html',
+  styleUrls: ['./all-blogs.component.scss']
+})
+export class AllBlogsComponent {
+blogs: Blog[] = [
+  {
+    title: 'Boost Your Typing Speed: Essential Tips',
+    thumbnail: 'assets/blogs/typing-speed.jpg',
+    publishedDate: '2026-03-20',
+    link: '/improve-typing-speed'
+  },
+  {
+    title: 'The Science Behind Typing Accuracy',
+    thumbnail: 'assets/blogs/accuracy-science.jpg',
+    publishedDate: '2026-03-22',
+    link: '/sciece-behind-accuracy'
+  },
+  {
+    title: 'Top Online Typing Games to Improve Your Skills',
+    thumbnail: 'assets/blogs/programming-games.jpg',
+    publishedDate: '2026-03-24',
+    link: '/top-online-typing-games'
+  },
+  {
+    title: 'Join the Ultimate Typing Challenge',
+    thumbnail: 'assets/blogs/typing-challenge.jpg',
+    publishedDate: '2026-03-26',
+    link: '/typing-challenge'
+  },
+  {
+    title: 'Play & Learn: The Best Typing Game',
+    thumbnail: 'assets/blogs/typing-game.jpg',
+    publishedDate: '2026-03-28',
+    link: '/typing-game'
+  }
+];
+}
